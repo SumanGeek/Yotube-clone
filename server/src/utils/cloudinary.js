d2285e9d85c9892d91bb3e0b4729c79c9ffbf187
SumanGeek <niroulas542@gmail.com>
2024-01-17 19:53:44 +0545

    project setup completed

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: "drqxwkcn8",
+  api_key: "562419282666795",
+  api_secret: "MuVl5PBRmMkBSzti-anXsunsX7s",
+});
+
+//upload the file in cloudinary
+
+const uploadOnCloud = async (fileName) => {
+  try {
+    if (!fileName) return null;
+    //upload file
+    const response = await cloudinary.uploader.upload(fileName, {
+      resource_type: "auto",
+    });
+    //after upload has been done sucessfully
+    console.log("file has been uploaded sucessfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(fileName);
+    //remove the locally saved file as file uploading process has some issue
+    return null;
+  }
+};
+
+export { uploadOnCloud };
